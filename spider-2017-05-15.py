a3dc6edaf1ed651b419144f3ba1969018fc8843d
某雪之 <xzgwz521@qq.com>
2017-05-18 19:52:40 +0800

    venv

--- spider-2017-05-15.py
+++ spider-2017-05-15.py
@@ -23,10 +23,11 @@ def get_total_page(html):  # 获取总页数
     n = re.search(r'[0-9]{1,10}', first_page)
     if n:
         number = int(n.group())
+        print('总页数为 %d' % number)
         return number
 
 
-def get_img_urls(page):  # 获取第二页的所有图片地址，返回列表
+def get_img_urls(page):  # 获取当前页面的所有图片地址，返回列表
     img_srcs = []
     page_url = 'http://www.meizitu.com/' + '/a/' + str(page) + '.html'
     html = url_open(page_url)
@@ -34,14 +35,40 @@ def get_img_urls(page):  # 获取第二页的所有图片地址，返回列表
     img_div = soup.find_all('div', id="picture")
     img_soup = BeautifulSoup(str(img_div), "lxml")  # 返回的列表长度为1
     img_result = list(img_soup.find_all('img'))  # 返回的列表长度为图片数
+    img_url = re.compile('http:.*\.jpg')
     for each in img_result:
-        img_url = re.compile('http:.*\.jpg')
         m = re.search(img_url, str(each))
         if m:
             img_src = str(m.group())
             img_srcs.append(img_src)
 
-    return img_srcs
+    return img_srcs  # 返回图片链接的列表
+
+
+def save_img(img_srcs, current_page):
+    a = random.random()  # 设置0-1秒的随机时间，用作延时
+    count = 0  # 当前页面图片计数
+    for each in img_srcs:
+        filename = str(current_page) + '-'+str(count+1) + '.jpg'
+        if os.path.exists(filename):
+            size = os.path.getsize(filename)
+            if size == 0:
+                print('重新下载 %s' % filename)
+                with open(filename, 'wb') as f:
+                    picture = url_open(each)
+                    f.write(picture)
+            count += 1
+            print('第 %d 页的第 %d 张图片 %s 已存在，跳过' % (current_page, count, filename))
+            time.sleep(a)
+        else:
+            with open(filename, 'wb') as f:
+                picture = url_open(each)
+                f.write(picture)
+            count += 1
+            print('已下载第 %d 页的第 %d 张图片,文件名为 %s ' % (current_page, count, filename))
+            time.sleep(a*2)
+
+    return count
 
 
 def download():
@@ -55,22 +82,15 @@ def download():
     url = 'http://www.meizitu.com/'
     html = url_open(url)
     total_page = get_total_page(html)
-    print('总页面为 %d' % total_page)
-    total_num = 0
-    for page in range(0, total_page):
-        img_srcs = get_img_urls(total_page-page)
-        count = 0
-        for each in img_srcs:
-            filename = str(total_page-page) + '-'+str(count+1) + '.jpg'
-            with open(filename, 'wb') as f:
-                picture = url_open(each)
-                f.write(picture)
-            count += 1
-            print('已下载第 %d 页的第 %d 张图片,文件名为 %s ' % (total_page-page, count, filename))
+    total_num = 0  # 初始化下载图片总个数
+    a = random.random()
+    for p in range(0, total_page):
+        current_page = total_page-p  # 当前下载页面的页数
+        img_srcs = get_img_urls(current_page)  # 获取当前下载页面所有图片的链接， 类型为列表
+        count = save_img(img_srcs, current_page)
         total_num += count
-        print('当前一共下载了 %d 张图片' % total_num)
-        a = random.random() * 3
-        time.sleep(a)
+        print('当前已下载图片总数为： %d ' % total_num)
+        time.sleep(a*3)
 
 
 if __name__ == '__main__':
